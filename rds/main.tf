79df63bcc28f8e0f0cb531011c6d526b44015c2c
Obulreddy58 <113847423+Obulreddy58@users.noreply.github.com>
2026-04-18 17:04:13 +0100

    fix: add KMS key policies and scope IAM permissions for production readiness

--- rds/main.tf
+++ rds/main.tf
@@ -26,6 +26,47 @@ resource "aws_kms_alias" "rds" {
   target_key_id = aws_kms_key.rds.key_id
 }
 
+resource "aws_kms_key_policy" "rds" {
+  key_id = aws_kms_key.rds.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnableRootAccess"
+        Effect    = "Allow"
+        Principal = { AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root" }
+        Action    = "kms:*"
+        Resource  = "*"
+      },
+      {
+        Sid       = "AllowRDS"
+        Effect    = "Allow"
+        Principal = { Service = "rds.amazonaws.com" }
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:GenerateDataKey*",
+          "kms:ReEncrypt*",
+          "kms:CreateGrant"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid       = "AllowSecretsManager"
+        Effect    = "Allow"
+        Principal = { Service = "secretsmanager.amazonaws.com" }
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 ###############################################################################
 # Security Group
 ###############################################################################
